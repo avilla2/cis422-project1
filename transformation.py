83723c32cb4d58077629ce8c09ba6eeb566b95c9
Hyo Jae Shin <hyojaeshin@Jays-MacBook-Pro.local>
2021-01-24 13:30:13 -0800

    Minor update for tree

--- transformation.py
+++ transformation.py
@@ -8,31 +8,21 @@ anytree is a great tree library. I believe
 using this library will save us a lot of time
 '''
 
-class tree_node:
-	"""Tree Node"""
-	def __init__(self, ts):
-		self.data = ts
-		self.children = []
-
-class tf_tree(tree_node, NodeMixin): 
+class tf_tree(self, NodeMixin): 
 	'''
 	This is TransFormation tree
 	NodeMixin is a feature in anytree library
 	NodeMixin will add node feature to our functions
 	''' 
 
-	def create_tree(self, ts, name, parent=None, children=None): 
+	def create_tree(self, ts): 
 		'''
 		Create a new tree : initialize tree
 		'''
-		self.root = name
-		self.data = preprocesesing.read(ts)
-		self.parent = parent
-		if children:
-			self.children = children
-
+		#Node will make node automatically
+		return Node("root", data=preprocesesing.read(ts))
 	
-	def add_operate_tree(self, ts): 
+	def add_operator(preprocesesing, pnode, function, ts): 
 		'''
 		Add operators to the transformation tree
 		checking type compatibility of output/input of operators. 
@@ -40,42 +30,74 @@ class tf_tree(tree_node, NodeMixin):
 		
 		We need to get function from user which will be choices
 		Easist way to handle cornercases
+
+		We have to make sure that we only providing this function
+		when user create_tree first (user must provide ts)
+
+		pnode is for parent node
 		'''
-		if self.data:
-			if self.function:
-				ret = preprocesesing.function(ts)
-				return 
+		if function and ts:
+			return Node("operator", parent=pnode, op=function, data=ts)
 
 
-	def replace_process(self, ts, function): 
+	def replace_process(self, node, function, ts): 
 		'''
 		Replace a process step with a different operator. 
 		-difference(ts)   -scaling(ts)   -standardize(ts)   -logarithm(ts)   -cubic_root(ts)
-		from preprocesesing
+		from preprocesesing or it has to be on add_operator
 		'''
-		if function:
-			
+		if function and ts:
+			return Node("process", parent=pnode, op=function, data=ts)
 
-	def replicate_subTree(self):
+	def replicate_subtree(self):
 		'''
-		Copy and paste the one node, no following nodes : path above
+		Copy and paste the one node, no following nodes : path below
 		'''
+		return path
 
-	def replicate_treePath(self, function):
+	def replicate_tree_path(self, function):
 		'''
 		Different 
 		Copy and paste the path following the one node : entire path with diff op
-		'''
 
-	def add_subTree(self):
+		return path list
+		'''
+		return path
+
+	def add_subtree(self, node, ):
 		'''
 		Add a subtree to a node. 
+		Add path to input node children list
+		Make this node a sibling of current node
 		'''
 
-	def get_readyTree : Load/Save a tree. ###Load and save where?
+	def get_ready_tree(self, root): 
+		'''
+		Load/Save whole tree. ###Load and save where?
+		'''
+		return path
 
-	def get_readyPipeline : Load/Save a pipeline. 
+	def get_ready_pipeline(self, leaf):
+		'''
+		Load/Save a pipeline 
+		'''
+		return path 
 
-	def exec_tree : Execute a tree. 
+	def exec_tree(self, path): 
+		'''
+		Execute whole tree.
+		If tree is in default, use default for ML
+		if tree has pipeline, wait until they are all done
+		
+		Contains modeling and forecasting functions
+		then result present
+		'''
 
-	def exec_pipeline : Execute a pipeline.
+	def exec_pipeline(self, path): 
+		'''
+		Execute a pipeline.
+		If pipeline exists, run all
+
+		Contains modeling and forecasting functions
+		return all the results
+		'''
